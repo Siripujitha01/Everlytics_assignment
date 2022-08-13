98b28f46ed618f44e819167327c1d299189d79ab
pujitha <siripujitha9701@gamil.com>
2022-08-13 17:52:51 +0530

    everlytics

---
+++ validation.js
@@ -0,0 +1,34 @@
+let create = document.getElementById("create");
+let cancel = document.getElementById("cancel");
+function validate(s,s1) {
+  console.log(s)
+  const format = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+  if (
+    s === "" ||
+    s.length < 5 ||
+    s[0] === "_" ||
+    s[0] === "-" ||
+    s[0] === "+" ||
+    format.test(s)
+  ) {
+    document.getElementById(s1).style.visibility="visible"
+  }
+}
+create.addEventListener("click", function (e) {
+  e.preventDefault();
+  validate(document.getElementById("ics").value,"isc");
+  validate(document.getElementById("re").value,"er");
+  validate(document.getElementById("gsc").value,"gcs");
+  validate(document.getElementById("gbn").value,"gnb");
+  validate(document.getElementById("pi").value,"ip");
+  validate(document.getElementById("pn").value,"np");
+});
+cancel.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("ics").value = "";
+  document.getElementById("re").value = "";
+  document.getElementById("gsc").value = "";
+  document.getElementById("gbn").value = "";
+  document.getElementById("pi").value = "";
+  document.getElementById("pn").value = "";
+});
